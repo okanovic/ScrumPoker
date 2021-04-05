e492db09e29f734bed5fe460d14b396b12f65fc9
Okan TOPER <okan.toper94@gmail.com>
2021-04-06 01:05:56 +0300

    base react tailwind yapısı oluşturuldu

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <button className="bg-yellow-600 p-2 rounded-sm hover:bg-yellow-800 text-white">Gönder</button>
+    </div>
+  );
+}
+
+export default App;
