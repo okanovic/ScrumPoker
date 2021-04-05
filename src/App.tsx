a123593b7df5c83ea260b20d4355ea7a7e1eb3d1
Okan TOPER <okan.toper94@gmail.com>
2021-04-06 01:47:56 +0300

    config düzenlemeleri yapıldı

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <button className="bg-yellow-600 p-2 rounded-sm hover:bg-yellow-800 text-white">Gönder</button>
+      <div className="container mx-auto border-blue-600 border-b-2 border-fuchsia-600 mt-12">
+        <h1 className="text-center text-6xl p-4">OKAN <span className="bg-gray-900 text-white p-3 border-4 rounded-md border-gray-200">TOPER</span></h1>
+        <h2 className="text-center text-5xl p-4">Trainin React with Tailwind</h2>
+      </div>
+
+      <div className="container mx-auto border-red-600 border-b-2 border-fuchsia-600 mt-12">
+        <h1 className="text-center text-6xl p-4">OKAN TOPER</h1>
+        <h2 className="text-center text-5xl p-4">Trainin React with Tailwind</h2>
+      </div>
     </div>
+
   );
 }
 
