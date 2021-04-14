fcacb634037d311351381578c5501f65d6046ab2
Okan TOPER <okan.toper94@gmail.com>
2021-04-14 09:47:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ function App() {
   return (
     <div>
       <div className="container mx-auto border-blue-600 border-b-2 border-fuchsia-600 mt-12">
-        <h1 className="text-center text-6xl p-4">OKAN <span className="bg-gray-900 text-white p-3 border-4 rounded-md border-gray-200">TOPER</span></h1>
-        <h2 className="text-center text-5xl p-4">Trainin React with Tailwind</h2>
-      </div>
-
-      <div className="container mx-auto border-red-600 border-b-2 border-fuchsia-600 mt-12">
-        <h1 className="text-center text-6xl p-4">OKAN TOPER</h1>
-        <h2 className="text-center text-5xl p-4">Trainin React with Tailwind</h2>
-      </div>
+        <h1 className="text-center text-6xl p-4">WELCOME TO MOST FAMOUS SCRUM POKER APP</h1>
     </div>
 
   );
